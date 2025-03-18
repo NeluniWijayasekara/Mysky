a3b80ad0a1be5e51831202538a7be2cf26561de6
Nelu <neluni09@gmail.com>
2025-03-18 07:56:56 +0530

    add dependecies

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Mysky/", // Ensure this matches your repository name exactly
+  base: "/Mysky/",  // Ensure this matches your GitHub repository name
   plugins: [react()],
 })
