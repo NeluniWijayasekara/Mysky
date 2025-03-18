61f78717d718f0de9394f52bb00c7087f6913deb
Nelu <neluni09@gmail.com>
2025-03-18 21:28:13 +0530

    made website to host

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/Mysky/",  // Ensure this matches your GitHub repository name
   plugins: [react()],
-})
+});
