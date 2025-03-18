a176124a41dbaee8882c2b40f9a5437202ec3c44
Nelu <neluni09@gmail.com>
2025-03-18 07:49:55 +0530

    added vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Mysky/",  // Set the base to match your GitHub repository name
   plugins: [react()],
 })
